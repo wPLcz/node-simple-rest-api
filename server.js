1d78a32e14d19115fe797df114e374dff778b262
admin <admin@MacBook-Pro-admin.local>
2019-08-31 14:10:58 +0200

    tweaking port name and dependencies for heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const rootDirectory = require('./utility/path');
 const sourceFile = require(path.join(rootDirectory, 'json-folder', 'sample.json'))
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// creating default root route
+// creating default root route for api request
 app.get('/', (req, res, next) => {
     res.send(sourceFile);
 });
